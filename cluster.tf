88e7dfe56795aa4fc5bab3cb3e24280d521a3b8d
esmorais83 <esmorais83@gmail.com>
2024-07-06 21:13:38 -0300

    changed termination_minutes = 30

--- cluster.tf
+++ cluster.tf
@@ -1,9 +1,9 @@
 resource "databricks_cluster" "mycluster" {
-    num_workers = 1
+    num_workers = 0
     cluster_name = "mycluster_tfc"
     idempotency_token = "mycluster_tfc"
     spark_version = "11.3.x-cpu-ml-scala2.12"
     node_type_id = "i3.xlarge"
-    autotermination_minutes = 120
+    autotermination_minutes = 30
     data_security_mode = "SINGLE_USER"
 }
